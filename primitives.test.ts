f77b1f980f1540755b380d84cf1662013fbaa27d
samini <soufianeamini2002@gmail.com>
2024-07-18 23:12:58 +0100

    added tests for and and many

--- primitives.test.ts
+++ primitives.test.ts
@@ -1,4 +1,4 @@
-import { char, or, parser } from "./primitives"
+import { and, char, many, or, parser } from "./primitives"
 
 describe("char", () => {
   test(`parses the character 'h' from "hello"`, () => {
@@ -28,5 +28,36 @@ describe("or", () => {
     const p = or(char("h"))(char("e"))
     const res = parser(p)("world")
     expect(res).toStrictEqual([null, "world"])
+  })
+})
+
+describe("and", () => {
+  test(`parses 'h' followed by 'e' using and`, () => {
+    const p = and(char("h"))(char("e"))
+    const res = parser(p)("hello")
+    expect(res).toStrictEqual([["h", "e"], "llo"])
+  })
+
+  test(`doesn't parse if there's not 'h' strictly followed by 'e'`, () => {
+    const p = and(char("h"))(char("e"))
+    const res1 = parser(p)("yup")
+    expect(res1).toStrictEqual([null, "yup"])
+
+    const res2 = parser(p)("hup")
+    expect(res2).toStrictEqual([null, "hup"])
+  })
+})
+
+describe("many", () => {
+  test(`parses 'e' many times and stops`, () => {
+    const p = many(char("e"))
+    const res = parser(p)("eeek")
+    expect(res).toStrictEqual([["e", "e", "e"], "k"])
+  })
+
+  test(`doesn't parse a single 'e' and returns failure`, () => {
+    const p = many(char("e"))
+    const res = parser(p)("ayep")
+    expect(res).toStrictEqual([null, "ayep"])
   })
 })
